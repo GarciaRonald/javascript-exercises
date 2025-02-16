911448f9750133c4683841f00c6f5f689e817c6b
Ronald Garcia <ronaldgm@gmail.com>
2025-02-16 18:43:02 -0500

    Create function for 04_removeFromArray, Pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, remove) {
+    const newArr = arr.filter(item => item !== remove);
+    return newArr;
 };
 
 // Do not edit below this line
