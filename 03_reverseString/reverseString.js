656068d694ad56139a73d1585ff68090fd21b27c
Ronald Garcia <ronaldgm@gmail.com>
2025-02-16 18:28:52 -0500

    Create function for 03_reverseString, Pass first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const toArr = [...string];
+    const newArr = [];
+    let newStr = "";
+    for (const letter of toArr) {
+        newArr.unshift(letter);
+    }
+    newStr = newArr.join("");
+    return newStr;
 };
 
 // Do not edit below this line
