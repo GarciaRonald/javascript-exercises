acf98870c3a793064a5fb5b655bf783c58f2d1d3
Ronald Garcia <ronaldgm@gmail.com>
2025-02-16 17:58:05 -0500

    Add conditional to 02_repeatString to handle negative 'num' argument

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(string, num) {
     let newString = "";
+    if (num < 0) {
+        return "ERROR";
+    }
     for (let i = 1; i <= num; i++) {
         newString += string;
     }
