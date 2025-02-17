73495661ef8885f90f805126f89e18ab18563413
Ronald Garcia <ronaldgm@gmail.com>
2025-02-16 20:47:52 -0500

    Add function for 05_sumAll, Pass first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let sumAll = 0
+    for (let init = firstNum; init <= lastNum; init++) {
+        sumAll += init;
+    }
+    return sumAll;
 };
 
 // Do not edit below this line
