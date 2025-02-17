7ba4469e8b5ed98241b7c874a524d970c651351b
Ronald Garcia <ronaldgm@gmail.com>
2025-02-16 21:25:21 -0500

    Add logic to reject non-numbers for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,14 +3,15 @@ const sumAll = function(firstNum, lastNum) {
     let bigNum = 0;
     let sumAll = 0
 
+    if (typeof firstNum !== "number" || typeof lastNum !== "number") {
+        return "ERROR";
+    }
     if (firstNum < 0 || lastNum < 0) {
         return "ERROR";
     }
-
     if (firstNum % Math.floor(firstNum) !== 0 || lastNum % Math.floor(lastNum) !== 0) {
         return "ERROR";
     }
-
     if (firstNum > lastNum) {
         smallNum = lastNum;
         bigNum = firstNum;
