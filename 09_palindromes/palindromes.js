1285cafabdb82b705da2ce7acfccd688eacb0afe
Ronald Garcia <ronaldgm@gmail.com>
2025-03-02 21:38:05 -0500

    Add and test 09_palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const palin = str
+        .replaceAll("!", "")
+        .replaceAll(",", "")
+        .replaceAll(".", "")
+        .replaceAll(" ", "")
+        .toLowerCase();
+    for (let i = 0; i < palin.length; i++) {
+        if (palin[i] !== palin[palin.length - (i + 1)]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
