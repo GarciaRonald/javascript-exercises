82258c2bd77684434958e7f8491c9a6d1bf3fe82
Ronald Garcia <ronaldgm@gmail.com>
2025-03-02 22:10:57 -0500

    Add and test function for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const int = parseInt(num);
+    const fibby = [1, 1];
+    if (int === 0) {
+        return 0;
+    } else if (int > 0) {
+        for (let i = 2; i <= int; i++) {
+            fibby.push(fibby[i - 2] + fibby[i - 1]);
+        }
+        return fibby[int - 1];
+    } else if (int < 0) {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
