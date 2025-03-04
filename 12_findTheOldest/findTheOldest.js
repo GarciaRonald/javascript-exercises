9609cefa830380ebe75f4e92c5bee1b7842f2dc0
Ronald Garcia <ronaldgm@gmail.com>
2025-03-03 20:38:10 -0500

    Add and test function for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const persons = [];
+    const ages = [];
 
+    arr.map(person => {
+        if (person.yearOfDeath) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = 2025 - person.yearOfBirth;
+        }
+        persons.push(person);
+        ages.push(person.age);
+    });
+
+    const oldAge = Math.max(...ages);
+    const oldPerson = persons.filter(person => person.age === oldAge);
+    return oldPerson[0];
 };
 
 // Do not edit below this line
